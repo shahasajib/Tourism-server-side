780e8ed32dcdc032974890ff335ce53f24b73e4a
shahasajib <sajib.shaha99@gmail.com>
2021-10-31 16:28:35 +0600

    third

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 const cors = require("cors")
 
-const port = 5000
+const port = process.env.PORT || 5000
 // middleware
 app.use(cors());
 app.use(express.json());
